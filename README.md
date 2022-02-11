06e1f79eadb8cf8bad9b4fd8a6d98d8ed4afcf6f
Dylan Johnson <dylanjohnson@vista.local>
2022-02-10 23:17:48 -0700

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# temporal-elt
+# temporal-parent-child-activity
+Registers and runs two child workflows and two activites. 
+
+#run all tests 
+```make tests```
+
+#run worker & start 
+```make all```
+
+
---
+++ start/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/pborman/uuid"
+	"go.temporal.io/sdk/client"
+
+	app "temporal-weather/app"
+)
+
+func main() {
+	c, err := client.NewClient(client.Options{})
+	if err != nil {
+		log.Fatalln("unable to create Temporal client", err)
+	}
+	defer c.Close()
+	workflowID := "parent-workflow_" + uuid.New()
+	//Workflow One
+	//Runs ChildWorkflowOne which exectures GetHumidity()
+	workflowOptions := client.StartWorkflowOptions{
+		ID:        workflowID,
+		TaskQueue: "child-workflow",
+	}
+	workflowRun, err := c.ExecuteWorkflow(context.Background(), workflowOptions, app.ChildWorkflowOne)
+	if err != nil {
+		log.Fatalln("Unable to execute workflow", err)
+	}
+	log.Println("Started workflow",
+		"WorkflowID", workflowRun.GetID(), "RunID", workflowRun.GetRunID())
+
+	var result_1 float64
+	err = workflowRun.Get(context.Background(), &result_1)
+	if err != nil {
+		log.Fatalln("Failure getting workflow result", err)
+	}
+	log.Println("Workflow result child workflow 1", result_1)
+
+	//Workflow two
+	//Runs ChildWorkFlowTwo which exectures GetTemp()
+	var result_2 int
+	workflowRun, err = c.ExecuteWorkflow(context.Background(), workflowOptions, app.ChildWorkflowTwo)
+	if err != nil {
+		log.Fatalln("Unable to execture workflow", err)
+	}
+	log.Println("Started workflow",
+		"WorkflowID", workflowRun.GetID(), &result_2)
+
+	log.Println("Workflow result child workflow 2", result_2)
+
+}
